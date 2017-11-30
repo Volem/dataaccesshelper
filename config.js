582a12394830aa8ea39d570fb2954670a7f79973
Volkan Nazmi Metin <v.metin@triodor.eu>
2017-11-30 13:44:24 +0300

    Empty config added

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	mongodbconnection : '',
+	sqlServer : '',
+	sqlUser : '',
+	sqlPwd: '',
+	sqlDatabase: ''
+};
--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 'use strict';
-const config = require('../config');
+const config = require('./config');
 const mongoose = require('mongoose');
 try {
 	if (!mongoose.connection.readyState) {
