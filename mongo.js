1cfaec2fc5cd06f36602d46541ebd450d31e4ec4
Volkan Nazmi Metin <volkan.nazmi.metin@gmail.com>
2017-11-30 13:32:59 +0300

    Initial files

---
+++ mongo.js
@@ -0,0 +1,24 @@
+'use strict';
+const config = require('../config');
+const mongoose = require('mongoose');
+try {
+	if (!mongoose.connection.readyState) {
+		mongoose.Promise = require('bluebird');
+		mongoose.connect(config.mongodbconnection, {
+			socketTimeoutMS: 30000,
+			connectTimeoutMS: 30000,
+			keepAlive: 1000,
+			useMongoClient: true,
+		}, function (err) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('Connected to MongoDB.');
+			}
+		});
+	} else {
+		console.log('MongoDB connection is established already. No need to reconnect.');
+	}
+} catch (ex) {
+	console.log('Connection Exception : ' + ex.message);
+}
